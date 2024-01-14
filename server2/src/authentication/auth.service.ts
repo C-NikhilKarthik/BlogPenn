aa482e13dab6cc76d5f1d19350b2c5ef200b4d7d
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2024-01-14 19:36:34 +0530

    login

--- server2/src/authentication/auth.service.ts
+++ server2/src/authentication/auth.service.ts
@@ -3,15 +3,20 @@ import { UserRegistrationDto } from "./auth.dto";
 import { Users } from "../entities/User.entity";
 import { AppSataSource } from "..";
 
-export class AuthService{
-    private static userRepository:Repository<Users>;
+export class AuthService {
+  private static userRepository: Repository<Users>;
 
-    static async signUp(body:UserRegistrationDto):Promise<any>{
-        const myDataSource = AppSataSource
-        // const newUser =await this.userRepository.create(body);
-        console.log(body);
-        const dob = new Date();
-        body.dob=dob;
-        return await myDataSource.getRepository(Users).save(body)
-    }
+  static async signUp(body: UserRegistrationDto): Promise<any> {
+    const myDataSource = AppSataSource;
+    // const newUser =await this.userRepository.create(body);
+    console.log(body);
+    const dob = new Date();
+    body.dob = dob;
+    return await myDataSource.getRepository(Users).save(body);
+  }
+
+  static async logIn(body: UserRegistrationDto): Promise<any> {
+    const myDataSource = AppSataSource;
+    console.log(body);
+  }
 }
